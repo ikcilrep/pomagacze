93bacb4ba03c674a7c9a429845eb6ca31e5189bb
Jakub <jakub@cubepotato.eu>
2022-10-08 17:12:54 +0200

    begin implementing supabase

---
+++ lib/pages/login.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:supabase/supabase.dart';
+import 'package:pomagacze/components/auth_state.dart';
+import 'package:pomagacze/utils/constants.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends AuthState<LoginPage> {
+  bool _isLoading = false;
+  late final TextEditingController _emailController;
+
+  Future<void> _signIn() async {
+    await supabase.auth.signInWithProvider(
+      Provider.google,
+      options: AuthOptions(redirectTo: 'https://urvsgbuuxnpnefnxtoha.supabase.co/auth/v1/callback')
+    );
+
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _emailController = TextEditingController();
+  }
+
+  @override
+  void dispose() {
+    _emailController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Sign In')),
+      body: ListView(
+        padding: const EdgeInsets.symmetric(vertical: 18, horizontal: 12),
+        children: [
+          ElevatedButton(
+            onPressed: _isLoading ? null : _signIn,
+            child: Text(_isLoading ? 'Loading' : 'Login with Google'),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
