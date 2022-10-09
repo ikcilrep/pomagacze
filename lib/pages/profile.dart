1b0e31b75841969eba211db7163428e262885492
Jakub <jakub@cubepotato.eu>
2022-10-09 22:22:37 +0200

    improve profile page

--- lib/pages/profile.dart
+++ lib/pages/profile.dart
@@ -1,70 +1,44 @@
-import 'package:date_time_picker/date_time_picker.dart';
+import 'package:age_calculator/age_calculator.dart';
+import 'package:avatars/avatars.dart';
 import 'package:flutter/material.dart';
-import 'package:gender_picker/source/enums.dart';
-import 'package:open_location_picker/open_location_picker.dart';
 import 'package:pomagacze/components/auth_required_state.dart';
-import 'package:pomagacze/components/buttons.dart';
-import 'package:pomagacze/db/users.dart';
+import 'package:pomagacze/db/db.dart';
 import 'package:pomagacze/models/user_profile.dart';
+import 'package:pomagacze/pages/edit_profile.dart';
 import 'package:pomagacze/utils/constants.dart';
-import 'package:pomagacze/utils/string_extensions.dart';
-import 'package:supabase_flutter/supabase_flutter.dart';
-import 'package:pomagacze/utils/snackbar.dart';
 import 'package:pomagacze/utils/gender_serializing.dart';
+import 'package:pomagacze/utils/snackbar.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 class ProfilePage extends StatefulWidget {
   const ProfilePage({Key? key}) : super(key: key);
 
   @override
-  ProfilePageState createState() => ProfilePageState();
+  State<ProfilePage> createState() => _ProfilePageState();
 }
 
-class ProfilePageState extends AuthRequiredState<ProfilePage> {
-  final _usernameController = TextEditingController();
-  var _isLoading = true;
+class _ProfilePageState extends AuthRequiredState<ProfilePage> {
   var _isInitialized = false;
   late UserProfile userProfile;
 
   Future<void> _fetchProfile(String userId) async {
-    setState(() {
-      _isLoading = true;
-    });
-
     userProfile = await UsersDB.getById(userId).catchError((err) {
       if (mounted) {
         context.showErrorSnackBar(message: (err as PostgrestError).message);
       }
     });
 
-    _usernameController.text = userProfile.name ?? '';
-
     setState(() {
-      _isLoading = false;
       _isInitialized = true;
     });
   }
 
-  Future<void> _saveChanges() async {
-    setState(() {
-      _isLoading = true;
-    });
-
-    userProfile.name = _usernameController.text;
-
-    try {
-      await UsersDB.update(userProfile);
-      if (mounted) {
-        context.showSnackBar(message: 'Udało się pomyślnie zapisać zmiany!');
-      }
-    } catch (err) {
-      if (mounted) {
-        context.showErrorSnackBar(message: (err as PostgrestError).message);
-      }
+  @override
+  void onAuthenticated(Session session) {
+    final user = session.user;
+    if (user != null) {
+      _fetchProfile(user.id);
     }
-
-    setState(() {
-      _isLoading = false;
-    });
   }
 
   Future<void> _signOut() async {
@@ -76,82 +50,51 @@ class ProfilePageState extends AuthRequiredState<ProfilePage> {
   }
 
   @override
-  void onAuthenticated(Session session) {
-    final user = session.user;
-    if (user != null) {
-      _fetchProfile(user.id);
-    }
-  }
-
-  @override
-  void dispose() {
-    _usernameController.dispose();
-    super.dispose();
-  }
-
-  @override
   Widget build(BuildContext context) {
     if (!_isInitialized) {
       return const Center(child: CircularProgressIndicator());
     }
 
-    return ListView(
-      padding: const EdgeInsets.symmetric(vertical: 18, horizontal: 12),
-      children: [
-        TextFormField(
-          controller: _usernameController,
-          decoration: const InputDecoration(labelText: 'Nazwa użytkownika'),
-        ),
-        const SizedBox(height: 12),
-        DropdownButtonFormField<Gender>(
-          items: Gender.values.map((value) {
-            return DropdownMenuItem<Gender>(
-              value: value,
-              child: Text(value.display().capitalizeFirst()),
-            );
-          }).toList(),
-          decoration: const InputDecoration(labelText: 'Płeć'),
-          value: userProfile.gender,
-          onChanged: (_) {},
-        ),
-        const SizedBox(height: 12),
-        DateTimePicker(
-          type: DateTimePickerType.date,
-          dateMask: 'd MMM, yyyy',
-          initialValue: userProfile.birthDate?.toString(),
-          firstDate: DateTime(1900),
-          lastDate: DateTime.now(),
-          // icon: const Icon(Icons.event),
-          dateLabelText: 'Data urodzenia',
-          onChanged: (dateTimeString) {
-            userProfile.birthDate = DateTime.tryParse(dateTimeString);
-          },
-          decoration: const InputDecoration(labelText: 'Data urodzenia'),
-        ),
-        const SizedBox(height: 12),
-        OpenMapPicker(
-          initialValue: userProfile.location,
-          decoration: const InputDecoration(
-            labelText: "Miejsce zamieszkania",
+    return Container(
+      padding: const EdgeInsets.all(20),
+      child: Column(
+        children: [
+          Row(
+            children: [
+              Avatar(
+                  shape: AvatarShape.circle(25),
+                  name: userProfile.name,
+                  placeholderColors: [
+                    Theme.of(context).colorScheme.primary,
+                  ]),
+              const SizedBox(width: 15),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(userProfile.name!,
+                      style: Theme.of(context).textTheme.headline6),
+                  Text('${userProfile.gender?.display()} • ${AgeCalculator.age(userProfile.birthDate!).years} l.')
+                ],
+              ),
+              Expanded(child: Container()),
+              IconButton(
+                  onPressed: () {
+                    showModalBottomSheet(
+                        context: context,
+                        isScrollControlled: true,
+                        builder: (context) {
+                          return Wrap(children: [
+                            EditProfilePage(userProfile: userProfile)
+                          ]);
+                        });
+                  },
+                  icon: const Icon(Icons.edit))
+            ],
           ),
-          onChanged: (FormattedLocation? newValue) {
-            userProfile.location = newValue;
-          },
-        ),
-        const SizedBox(height: 18),
-        ElevatedButton(
-          onPressed: _saveChanges,
-          style: primaryButtonStyle(context),
-          child: _isLoading
-              ? Transform.scale(
-                  scale: 0.7,
-                  child: CircularProgressIndicator(
-                      color: Theme.of(context).colorScheme.onPrimary))
-              : const Text('Zapisz'),
-        ),
-        const SizedBox(height: 12),
-        OutlinedButton(onPressed: _signOut, child: const Text('Wyloguj się')),
-      ],
+          const SizedBox(height: 20),
+          OutlinedButton(onPressed: _signOut, child: const Text('Wyloguj się')),
+        ],
+      ),
     );
   }
 }
