2b634ebdd69ac8d7c3865ea14d462a491ab8903b
ikcilrep <frajerzycki@protonmail.com>
2022-10-16 00:35:30 +0200

    Enable opening events from activities

--- lib/pages/activities.dart
+++ lib/pages/activities.dart
@@ -1,7 +1,9 @@
+import 'package:animations/animations.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:pomagacze/pages/event_details.dart';
 import 'package:pomagacze/state/activities.dart';
 import 'package:pomagacze/utils/date_extensions.dart';
 
@@ -14,16 +16,29 @@ class ActivitiesPage extends ConsumerWidget {
         ref.watch(friendsAndUserActivitiesProvider);
     return friendsAndUserActivities.when(
         data: (friendsAndUserActivities) {
-          friendsAndUserActivities.sort((a, b) => b.createdAt.compareTo(a.createdAt));
+          friendsAndUserActivities
+              .sort((a, b) => b.createdAt.compareTo(a.createdAt));
           return ListView.builder(
               itemCount: friendsAndUserActivities.length,
               itemBuilder: (context, index) {
                 final activity = friendsAndUserActivities[index];
-                return ListTile(
-                  title: Text(
-                      '${activity.user.name} dołączył do "${activity.event.title}"'),
-                  subtitle: Text(activity.createdAt.displayable()),
-                );
+                return OpenContainer<bool>(
+                    tappable: true,
+                    transitionType: ContainerTransitionType.fadeThrough,
+                    transitionDuration: const Duration(milliseconds: 350),
+                    openBuilder: (BuildContext context, VoidCallback _) =>
+                        EventDetails(activity.event),
+                    closedShape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(18)),
+                    closedElevation: 1.5,
+                    // transitionDuration: const Duration(seconds: 2),
+                    closedBuilder: (_, openContainer) {
+                      return ListTile(
+                        title: Text(
+                            '${activity.user.name} dołączył do "${activity.event.title}"'),
+                        subtitle: Text(activity.createdAt.displayable()),
+                      );
+                    });
               });
         },
         error: (err, stack) => Center(child: Text('Coś poszło nie tak: $err')),
