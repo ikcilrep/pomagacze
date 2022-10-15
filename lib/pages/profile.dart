4583f9d824a0ef103fd311ce58bcde25c726495e
ikcilrep <frajerzycki@protonmail.com>
2022-10-16 00:29:29 +0200

    Add recent activities of a user

--- lib/pages/profile.dart
+++ lib/pages/profile.dart
@@ -1,11 +1,16 @@
+import 'package:animations/animations.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:pomagacze/components/profile_action.dart';
 import 'package:pomagacze/components/user_profile_details.dart';
 import 'package:pomagacze/db/db.dart';
+import 'package:pomagacze/models/activity.dart';
 import 'package:pomagacze/models/user_profile.dart';
+import 'package:pomagacze/pages/event_details.dart';
+import 'package:pomagacze/state/activities.dart';
 import 'package:pomagacze/state/friendships.dart';
 import 'package:pomagacze/utils/constants.dart';
+import 'package:pomagacze/utils/date_extensions.dart';
 import 'package:pomagacze/utils/snackbar.dart';
 
 class ProfilePage extends ConsumerStatefulWidget {
@@ -34,12 +39,21 @@ class ProfilePageState extends ConsumerState<ProfilePage> {
       return [];
     }
 
-    var friendIds = ref.watch(friendsIdsProvider);
-    var outgoingFriendRequests = ref.watch(outgoingFriendRequestsProvider);
-    var incomingFriendRequests = ref.watch(incomingFriendRequestsProvider);
+    final friendIds = ref.watch(friendsIdsProvider);
+    final outgoingFriendRequests = ref.watch(outgoingFriendRequestsProvider);
+    final incomingFriendRequests = ref.watch(incomingFriendRequestsProvider);
+    final userActivities =
+        ref.watch(userActivitiesProvider(widget.userProfile.id));
 
     if (friendIds.valueOrNull?.contains(widget.userProfile.id) == true) {
       return [
+        ExpansionTile(title: const Text("Ostatnie wydarzenia"), children: [
+          userActivities.when(
+              data: _buildUserActivities,
+              error: (err, stack) =>
+                  Center(child: Text('Coś poszło nie tak: $err')),
+              loading: () => const Center(child: CircularProgressIndicator()))
+        ]),
         ProfileAction(
             title: const Text('Usuń znajomego'),
             icon: const Icon(Icons.person_remove),
@@ -128,5 +142,31 @@ class ProfilePageState extends ConsumerState<ProfilePage> {
         },
       )
     ];
+  }
+
+  Widget _buildUserActivities(List<Activity> activities) {
+    activities.sort((a, b) => b.createdAt.compareTo(a.createdAt));
+    return ListView.builder(
+      shrinkWrap: true,
+      physics: const NeverScrollableScrollPhysics(),
+      itemCount: activities.length,
+      itemBuilder: (BuildContext context, int index) {
+        return OpenContainer<bool>(
+            tappable: true,
+            transitionType: ContainerTransitionType.fadeThrough,
+            transitionDuration: const Duration(milliseconds: 350),
+            openBuilder: (BuildContext context, VoidCallback _) =>
+                EventDetails(activities[index].event),
+            closedShape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(18)),
+            closedElevation: 1.5,
+            // transitionDuration: const Duration(seconds: 2),
+            closedBuilder: (_, openContainer) => ListTile(
+                  title: Text(activities[index].event.title),
+                  subtitle: Text(
+                      "Dołączono ${activities[index].createdAt.displayable()}"),
+                ));
+      },
+    );
   }
 }
--- lib/state/activities.dart
+++ lib/state/activities.dart
@@ -4,7 +4,8 @@ import 'package:pomagacze/models/activity.dart';
 import 'package:pomagacze/state/friendships.dart';
 import 'package:pomagacze/utils/constants.dart';
 
-final friendsAndUserActivitiesProvider = FutureProvider<List<Activity>>((ref) async {
+final friendsAndUserActivitiesProvider =
+    FutureProvider<List<Activity>>((ref) async {
   final userId = supabase.auth.user()?.id ?? '';
   final friendsIds = await ref.watch(friendsIdsProvider.future);
   final activities = <Activity>[];
@@ -14,3 +15,6 @@ final friendsAndUserActivitiesProvider = FutureProvider<List<Activity>>((ref) as
 
   return activities;
 });
+
+final userActivitiesProvider = FutureProvider.family<List<Activity>, String>(
+    (ref, userId) async => ActivitiesDB.getAllByUserId(userId));
