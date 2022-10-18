bb3d512e8a892c72d101b0bcc5eef27347389c0b
Jakub <jakub@cubepotato.eu>
2022-10-18 21:23:48 +0200

    badges

--- lib/components/volunteers_badge.dart
+++ lib/components/volunteers_badge.dart
@@ -2,7 +2,6 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:pomagacze/models/help_event.dart';
 
-import '../models/volunteer.dart';
 class VolunteersBadge extends StatelessWidget {
   final HelpEvent event;
 
@@ -14,22 +13,22 @@ class VolunteersBadge extends StatelessWidget {
         borderRadius: BorderRadius.circular(20),
         child: Container(
             decoration: BoxDecoration(
-                color: Theme.of(context).colorScheme.primary.withOpacity(0.7),
+                color: Theme.of(context).colorScheme.secondary.withOpacity(0.8),
                 border: Border.all(color: Colors.black12, width: 0),
                 borderRadius: const BorderRadius.all(Radius.circular(20))),
             child: Center(
               child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 5),
+                padding: const EdgeInsets.symmetric(horizontal: 7, vertical: 5),
                 child: Row(
                   children: [
-                    const Icon(Icons.group, color: Colors.white, size: 20),
+                    const Icon(Icons.group, color: Colors.black87, size: 20),
                     const SizedBox(width: 5),
                     Text(
                       "${event.volunteers.length}/${event.maximalNumberOfVolunteers}",
                       style: Theme.of(context)
                           .textTheme
-                          .bodyText1
-                          ?.copyWith(color: Colors.white),
+                          .bodyText2
+                          ?.copyWith(color: Colors.black87),
                     ),
                   ],
                 ),
