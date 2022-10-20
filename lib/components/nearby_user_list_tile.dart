9e866a061f7da9ecfd51c197ca1aa273819be4c9
ikcilrep <frajerzycki@protonmail.com>
2022-10-20 19:00:12 +0200

    Show a snackbar message when participation is successfully confirmed

--- lib/components/nearby_user_list_tile.dart
+++ lib/components/nearby_user_list_tile.dart
@@ -5,33 +5,52 @@ import 'package:pomagacze/db/volunteers.dart';
 import 'package:pomagacze/models/help_event.dart';
 import 'package:pomagacze/models/volunteer.dart';
 import 'package:pomagacze/state/events.dart';
+import 'package:pomagacze/utils/snackbar.dart';
 
-class NearbyVolunteerListTile extends ConsumerWidget {
+class NearbyVolunteerListTile extends ConsumerStatefulWidget {
   final Volunteer volunteer;
 
   const NearbyVolunteerListTile({Key? key, required this.volunteer})
       : super(key: key);
 
+  @override
+  ConsumerState<ConsumerStatefulWidget> createState() =>
+      NearbyVolunteerListTileState();
+}
+
+class NearbyVolunteerListTileState
+    extends ConsumerState<NearbyVolunteerListTile> {
   AutoDisposeFutureProvider<HelpEvent> get eventProvider {
-    return eventFutureProvider(volunteer.eventId);
+    return eventFutureProvider(widget.volunteer.eventId);
   }
 
-  Future<void> _confirmVolunteerParticipation(WidgetRef ref) async {
-    volunteer.isParticipationConfirmed = true;
-    await VolunteersDB.update(volunteer);
-    ref.refresh(eventProvider);
+  Future<void> _confirmVolunteerParticipation(BuildContext context) async {
+    try {
+      widget.volunteer.isParticipationConfirmed = true;
+      await VolunteersDB.update(widget.volunteer);
+      ref.refresh(eventProvider);
+      if (mounted) {
+        context.showSnackBar(
+            message: 'Udało się potwierdzić uczestnictwo wolontariusza!');
+      }
+    } catch (e) {
+      if (mounted) {
+        context.showErrorSnackBar(
+            message: 'Nie udało się potwierdzić uczestnictwa wolontariusza.');
+      }
+    }
   }
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  Widget build(BuildContext context) {
     return Material(
       child: ListTile(
         contentPadding: const EdgeInsets.symmetric(horizontal: 16, vertical: 5),
-        title: Text(volunteer.profile!.name ?? ''),
-        leading: UserAvatar(volunteer.profile!),
+        title: Text(widget.volunteer.profile!.name ?? ''),
+        leading: UserAvatar(widget.volunteer.profile!),
         trailing: IconButton(
             icon: const Icon(Icons.done, color: Colors.teal),
-            onPressed: () => _confirmVolunteerParticipation(ref)),
+            onPressed: () => _confirmVolunteerParticipation(context)),
       ),
     );
   }
