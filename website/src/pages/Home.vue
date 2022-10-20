22c76e8d2d92652ffc8863b99afa931e2a6436ea
Jakub <jakub@cubepotato.eu>
2022-10-21 00:02:50 +0200

    website: new app release

--- website/src/pages/Home.vue
+++ website/src/pages/Home.vue
@@ -2,10 +2,10 @@
   <v-container class="d-flex align-center flex-column text-center mt-8 pb-10">
     <h1 class="text-h3 mb-2">Pomagacze</h1>
     <h2 class="text-h6">Aplikacja Pomagacze zachęca do pomagania, bycia wolontariuszem,<br>zmieniania swojego otoczenia na lepsze</h2>
-    <img :src="imgUrl" width="300"/>
-    <v-btn class="mt-3" color="primary" rounded="xl" size="x-large" to="/install">POBIERZ
+    <v-btn class="mt-4" color="primary" rounded="xl" size="x-large" to="/install">POBIERZ
       <v-icon class="ml-2">mdi-download</v-icon>
     </v-btn>
+    <img :src="imgUrl" width="300" class="mt-4"/>
   </v-container>
 </template>
 
