9a3b334f5df621952caa289c3e6afbcbd1366f4e
ikcilrep <ikcilrep@outlook.com>
2022-10-23 13:05:25 +0200

    Improve activities text

--- lib/pages/users/activities.dart
+++ lib/pages/users/activities.dart
@@ -3,9 +3,13 @@ import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:gender_picker/source/enums.dart';
 import 'package:pomagacze/components/general/error_with_action.dart';
+import 'package:pomagacze/models/activity.dart';
+import 'package:pomagacze/models/user_profile.dart';
 import 'package:pomagacze/pages/events/event_details.dart';
 import 'package:pomagacze/state/activities.dart';
+import 'package:pomagacze/state/users.dart';
 import 'package:pomagacze/utils/date_extensions.dart';
 
 class ActivitiesPage extends ConsumerWidget {
@@ -13,8 +17,9 @@ class ActivitiesPage extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final activities =
-        ref.watch(activitiesProvider);
+    final activities = ref.watch(activitiesProvider);
+
+    final currentUserAsyncValue = ref.watch(currentUserProvider);
 
     return activities.when(
         data: (data) {
@@ -27,8 +32,7 @@ class ActivitiesPage extends ConsumerWidget {
                 actionText: 'Dodaj znajomych');
           }
           return RefreshIndicator(
-            onRefresh: () =>
-                ref.refresh(activitiesProvider.future),
+            onRefresh: () => ref.refresh(activitiesProvider.future),
             child: ListView.builder(
                 itemCount: data.length,
                 padding: const EdgeInsets.symmetric(vertical: 12),
@@ -48,8 +52,11 @@ class ActivitiesPage extends ConsumerWidget {
                       closedBuilder: (_, openContainer) {
                         return ListTile(
                           onTap: openContainer,
-                          title: Text(
-                              '${activity.user.name} dołączył do "${activity.event.title}"'),
+                          title: currentUserAsyncValue.when(data: (currentUser) {
+                            return Text(_getActivityJoinedText(activity, currentUser, ref));
+                          }, error: (err,stack) {
+                            return Container();
+                          }, loading: () => const Center(child: CircularProgressIndicator())),
                           subtitle: Text(activity.createdAt.displayable()),
                         );
                       });
@@ -63,5 +70,21 @@ class ActivitiesPage extends ConsumerWidget {
             },
             actionText: 'Odśwież'),
         loading: () => const Center(child: CircularProgressIndicator()));
+  }
+
+  String _getActivityJoinedText(
+      Activity activity, UserProfile currentUser, WidgetRef ref) {
+    if (currentUser.gender == Gender.Female) {
+      if (currentUser.id == activity.user.id) {
+        return 'Dołączyłaś do "${activity.event.title}"';
+      }
+
+      return '${activity.user.name} dołączyła do "${activity.event.title}"';
+    }
+    if (currentUser.id == activity.user.id) {
+      return 'Dołączyłeś do "${activity.event.title}"';
+    }
+
+    return '${activity.user.name} dołączył do "${activity.event.title}"';
   }
 }
