7ce1ff0d9b6edea1ef76bf3d5397e83e47c23a38
Jakub <jakub@cubepotato.eu>
2022-10-23 11:40:13 +0200

    website nearby connections video

--- website/src/pages/Home.vue
+++ website/src/pages/Home.vue
@@ -49,7 +49,7 @@
         <iframe class="rounded-lg" src="https://player.vimeo.com/video/762995817?h=79c66b2416&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen style="position:absolute;top:0;left:0;width:100%;height:100%;" title="Decorporation"></iframe>
       </div>
       <div style="padding:56.25% 0 0 0;position:relative;" class="mt-5">
-        <iframe class="rounded-lg" src="https://player.vimeo.com/video/762995817?h=79c66b2416&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen style="position:absolute;top:0;left:0;width:100%;height:100%;" title="Decorporation"></iframe>
+        <iframe class="rounded-lg" src="https://player.vimeo.com/video/763070883?h=79c66b2416&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen style="position:absolute;top:0;left:0;width:100%;height:100%;" title="Decorporation"></iframe>
       </div>
     </div>
     <component :is='"script"' src="https://player.vimeo.com/api/player.js"></component>
